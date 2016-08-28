27443ec1bacef825d536d6c7caf081a467d2ba5d
Matt York <my0373@gmail.com>
2016-08-28 15:04:50 +0100

    Updated dns logic in the generated script

--- roles/idm/templates/install-idm.sh
+++ roles/idm/templates/install-idm.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
 ipa-server-install \
-	{% if setup_dns %}
+    -U  \
+	{% if setup_dns and use_dns_forwarders %}
+	--setup-dns  \
+    --forwarder {{ dns_forwarder_ip }}  \
+    --reverse-zone {{ reversename|lower }}  \
+    {% elif setup_dns and not use_dns_forwarders %}
+	--no-forwarders  \
+	--setup-dns  \
+	--reverse-zone {{ reversename|lower }}  \
+	{% elif setup_dns %}
 	--setup-dns  \
 	{% endif %}
-	{% if use_dns_forwarders %}
-    --forwarder {{ dns_forwarder_ip }}  \
-	{% else %}
-	--no-forwarders  \
-	{% endif %}
+
 	--ds-password {{ idm_ds_password }}  \
 	--admin-password {{ idm_admin_password }}  \
 	--realm	{{ realm|upper }}  \
 	--hostname {{ hostname|lower }}.{{ domain|lower }}  \
 	--domain  {{ domain|lower }}  \
-	--reverse-zone {{ reversename|lower }}  \
 	--ip-address {{ ip_address }}
